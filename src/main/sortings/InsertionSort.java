b92c4eaf25cc6ccd26315a3939007c2bebe590a7
Bruno Alliguieri <bruno.dias@ceresinvestimentos.com>
2021-05-28 13:16:26 -0300

    InsertionSort implemented.

---
+++ src/main/sortings/InsertionSort.java
@@ -0,0 +1,28 @@
+package main.sortings;
+
+import main.contracts.Sorter;
+import main.jalist.ArrayBasics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InsertionSort extends ArrayBasics implements Sorter {
+    public void sort(List<Integer> listToSort) {
+        for (var i = 1; i < listToSort.size(); i++) {
+            for (var j = 0; j <= i; j ++) {
+                if (listToSort.get(i) < listToSort.get(j)) {
+                    move(listToSort, i, j);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        var sorter = new InsertionSort();
+        List<Integer> numbers = new ArrayList<>(List.of(1098273,9, 6, 5, 0, 8, 2, 7, 1, 3, 4, 999));
+
+        sorter.sort(numbers);
+
+        System.out.printf(numbers.toString());
+    }
+}
