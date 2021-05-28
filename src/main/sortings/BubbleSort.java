531a763a5b515cbc1324d2c2b4d54363438d6a90
Bruno Alliguieri <bruno.dias@ceresinvestimentos.com>
2021-05-28 13:16:01 -0300

    BubbleSort and it's recursive versions implemented.

---
+++ src/main/sortings/BubbleSort.java
@@ -0,0 +1,46 @@
+package main.sortings;
+
+import main.contracts.Sorter;
+import main.jalist.ArrayBasics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BubbleSort extends ArrayBasics implements Sorter {
+    @Override
+    public void sort(List<Integer> list) {
+        for (var j = 0; j < list.size(); j++) {
+            for (var i = 0; i < list.size(); i++) {
+                if (i < list.size() - 1) {
+                    if (list.get(i + 1) < list.get(i)) {
+                        swap(list, i);
+                    }
+                }
+            }
+        }
+    }
+
+    public void recursiveSort(List<Integer> list) {
+        var swapped = false;
+        for (var i = 0; i < list.size(); i++) {
+            if (i < list.size() - 1) {
+                if (list.get(i + 1) < list.get(i)) {
+                    swapped = true;
+                    move(list, (i + 1), i);
+                }
+            }
+        }
+
+        if (swapped) {
+            recursiveSort(list);
+        }
+    }
+
+    public static void main(String[] args) {
+        var list = new ArrayList<>(List.of(101, 3, 2, 1, 99));
+
+        new BubbleSort().recursiveSort(list);
+
+        System.out.println(list);
+    }
+}
